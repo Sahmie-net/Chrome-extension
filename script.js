1e06225d7376c30d23ca0e199119589d43a5d94b
Sahmie-net <sahmie365@gmail.com>
2026-07-21 01:21:09 +0100

    Improve the performance of the app extension

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
-
-let inputBtn = document.getElementById("input-btn")
 inputBtn.addEventListener("click", function() {
-    console.log("Button Clicked")
+    myLeads.push(inputEl.value)
+    renderLeads()
 })
+
+
+function renderLeads() {
+    let listItems = ""
+
+    for(i = 0; i < myLeads.length; i++) {
+    listItems += "<li>" + myLeads[i] + "</li>"
+    } 
+
+    ulEl.innerHTML = listItems
+}
